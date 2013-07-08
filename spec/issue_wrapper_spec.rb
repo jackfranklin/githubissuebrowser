6a5ac5941bd0d590520881ab0a27343cf0ff30b3
Jack Franklin <jack@jackfranklin.net>
2013-07-08 15:00:36 +0100

    add specs for current functionality

--- spec/issue_wrapper_spec.rb
+++ spec/issue_wrapper_spec.rb
@@ -27,6 +27,34 @@ describe IssueWrapper do
       expect(@wrap.issues.first.class).to eq(Issue)
       expect(@wrap.issues.length).to eq(3)
     end
+    it "parses out owners" do
+      expect(@wrap.owners).to eq(["jackfranklin", "tester"])
+    end
+    it "parses out names" do
+      expect(@wrap.names).to eq(["test", "test2", "repo1"])
+    end
   end
 
+  describe "filtering by owner" do
+    it "correctly filters" do
+      @wrap.filter_by_owner("tester")
+      expect(@wrap.issues.length).to eq(1)
+      expect(@wrap.issues.first.name).to eq("repo1")
+    end
+
+    it "can restore all issues" do
+      @wrap.filter_by_owner("tester")
+      expect(@wrap.issues.length).to eq(1)
+      @wrap.restore
+      expect(@wrap.issues.length).to eq(3)
+    end
+  end
+
+  describe "filtering by name" do
+    it "correctly filters" do
+      @wrap.filter_by_name("test")
+      expect(@wrap.issues.length).to eq(1)
+      expect(@wrap.issues.first.owner).to eq("jackfranklin")
+    end
+  end
 end
